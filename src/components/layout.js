5ebb2ead221a0bd2012d3dcf05963e5c33b66dbe
James Castle <jcastle0@gmail.com>
2018-12-08 09:18:29 -0500

    Added SASS Plugin

--- src/components/layout.js
+++ src/components/layout.js
@@ -4,7 +4,8 @@ import Helmet from 'react-helmet'
 import { StaticQuery, graphql } from 'gatsby'
 
 import Header from './header'
-import './layout.css'
+// import './layout.css'
+import './layout.scss'
 
 const Layout = ({ children }) => (
   <StaticQuery
