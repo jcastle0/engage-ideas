1e0c48de98dbb22237c271aa8193d7cc665f801a
James Castle <jcastle0@gmail.com>
2018-12-09 11:52:28 -0500

    Added SCSS Design Tokens and Normalize CSS

--- src/components/layout.js
+++ src/components/layout.js
@@ -4,8 +4,7 @@ import Helmet from 'react-helmet'
 import { StaticQuery, graphql } from 'gatsby'
 
 import Header from './header'
-// import './layout.css'
-import './layout.scss'
+import '../styles/layout.scss'
 
 const Layout = ({ children }) => (
   <StaticQuery
@@ -31,12 +30,6 @@ const Layout = ({ children }) => (
         </Helmet>
         <Header siteTitle={data.site.siteMetadata.title} />
         <div
-          style={{
-            margin: '0 auto',
-            maxWidth: 960,
-            padding: '0px 1.0875rem 1.45rem',
-            paddingTop: 0,
-          }}
         >
           {children}
         </div>
---
+++ src/pages/styles.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+import Layout from '../components/layout'
+// import Image from '../components/image'
+
+const IndexPage = () => (
+  <Layout>
+    <h1>A page to demo styles</h1>
+    <h1>This as an H1</h1>
+    <h2>This as an H2</h2>
+    <h3>This as an H3</h3>
+    <h4>This as an H4</h4>
+    <h5>This as an H5</h5>
+    <h6>This as an H6</h6>
+  </Layout>
+)
+
+export default IndexPage
