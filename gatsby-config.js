028b7b21ce462d65084011978db18d10359990e6
James Castle <jcastle@uga.edu>
2018-12-11 11:19:15 -0500

    Trying to iterate over array in JSON File

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,9 @@
 module.exports = {
   siteMetadata: {
-    title: 'Enagage Ideas',
+    title: `Engage Ideas`,
   },
   plugins: [
-    'gatsby-plugin-react-helmet',
+    `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -18,20 +18,27 @@ module.exports = {
         path: `${__dirname}/src/pages`,
       },
     },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/src/data`,
+      },
+    },
     `gatsby-transformer-remark`,
-    'gatsby-transformer-sharp',
-    'gatsby-plugin-sharp',
+    `gatsby-transformer-sharp`,
+    `gatsby-transformer-json`,
+    `gatsby-plugin-sharp`,
     `gatsby-plugin-sass`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'gatsby-starter-default',
-        short_name: 'starter',
-        start_url: '/',
-        background_color: '#663399',
-        theme_color: '#663399',
-        display: 'minimal-ui',
-        icon: 'src/images/favicon-256.png', // This path is relative to the root of the site.
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/favicon-256.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
--- src/pages/resume.js
+++ src/pages/resume.js
@@ -1,12 +1,43 @@
 import React from 'react'
-// import { Link } from 'gatsby'
-
+import { graphql } from 'gatsby'
 import Layout from '../components/layout'
 
-const SecondPage = () => (
-  <Layout>
-    <h1>Resume</h1>   
-  </Layout>
-)
+/* Trying to figure out how to iterate over array value in JSON file */
 
-export default SecondPage
+export default ( props ) => {
+  const resume = props.data.allResumeJson.edges;
+  return (
+    <Layout>
+      {resume.map((r, i) => {
+        const resumeData = r.node;
+        console.log(resumeData)
+        return (
+          <div key={i}>
+            <p>{resumeData.Name}</p>
+            <p>{resumeData.Experience.Title}</p>
+          </div>
+        )
+      }
+        )}
+    </Layout>
+  )
+}
+
+export const query = graphql`
+  query {
+    allResumeJson {
+      edges {
+        node {
+          Name
+          Experience {
+            Title
+            Organization
+            Start_Date
+            End_Date
+            Description
+          }
+        }
+      }
+    }
+  }
+`
